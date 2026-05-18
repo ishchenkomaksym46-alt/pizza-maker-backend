64a4ae8f541f0f5c3195fe84965d4b9bdb834c82
Max <ishchenkomaksym46@gmail.com>
2026-05-18 09:26:57 +0300

    added CRUD for slices

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { SlicesModule } from './slices/slices.module';
 import {ConfigModule} from "@nestjs/config";
-import {PrismaService} from "./prisma.service";
 
 @Module({
   imports: [ConfigModule.forRoot({
--- src/slices/slices.controller.ts
+++ src/slices/slices.controller.ts
@@ -1,5 +1,7 @@
-import {Controller, Get} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Post, Put, Query} from '@nestjs/common';
 import { SlicesService } from './slices.service';
+import {CreateSlicesDto} from "../types/slices.dto";
+import type {UpdateSlicesDto} from "../types/slices.dto";
 
 @Controller('slices')
 export class SlicesController {
@@ -8,5 +10,20 @@ export class SlicesController {
   @Get()
   getAllSlices() {
     return this.slicesService.getAllSlices();
+  }
+
+  @Post('create')
+  createSlice(@Body() dto: CreateSlicesDto) {
+    return this.slicesService.createSlice(dto);
+  }
+
+  @Put('update')
+  updateSlice(@Body() dto: UpdateSlicesDto, @Query('id') id: string) {
+    return this.slicesService.updateSlice(dto, id);
+  }
+
+  @Delete('delete')
+  deleteSlice(@Query('id') id: string) {
+    return this.slicesService.deleteSlice(id);
   }
 }
